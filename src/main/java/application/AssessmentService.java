c1acc01b94bf7a889537f12972c0c5b2e56e5c11
lahjeq <bissic.billy@gmail.com>
2021-08-12 01:41:33 -0500

    adding swagger and project cleanup

--- src/main/java/application/AssessmentService.java
+++ src/main/java/application/AssessmentService.java
@@ -3,6 +3,8 @@
  */
 package application;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,10 +14,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * @author Billy Bissic
  *
  */
+@EnableSwagger2
 @SpringBootApplication
 public class AssessmentService extends SpringBootServletInitializer {
 
@@ -29,6 +39,29 @@ public class AssessmentService extends SpringBootServletInitializer {
 		return builder.sources(AssessmentService.class);
 	}
 	
+    @Bean
+    public Docket swaggerConfiguration() {
+    
+    	return new Docket(DocumentationType.SWAGGER_2)
+    			.select()
+    			.paths(PathSelectors.any())
+    			.apis(RequestHandlerSelectors.any())
+    			.build()
+    			.apiInfo(apiDetails());
+    }
+    
+    private ApiInfo apiDetails() {
+    	return new ApiInfo(
+    			"Yelp Wrapper",
+    			"API for interfacing with yelp.",
+    			"1.0",
+    			"Demo",
+    			new springfox.documentation.service.Contact("Billy Bissic", "https://github.com/billybissic", "billy@magnificenteyes.com"),
+    			"API License",
+    			"http://magnificenteyes.com/license",
+    			Collections.emptyList());
+    }
+    
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurerAdapter() {
